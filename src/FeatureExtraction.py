ceefd37dac7eb7aa4f97ad686e264d0025012df8
Zach Hayes <hayes.Zach@protonmail.com>
2024-10-20 16:22:26 -0700

    Load api key from config file
    - load api key from config file
    - provide example config file structure
    - remove unnecessary functions from FeatureExtraction
    - utilize helper functions in main and FeatureExtraction

--- src/FeatureExtraction.py
+++ src/FeatureExtraction.py
@@ -1,59 +1,16 @@
-import collections
-import math
-import string
-import os
-import re, textstat
+from helper import remove_special_chars, percentage_hard_words, average_sentence_length, average_syllables_per_word
 
+import textstat
 import matplotlib.pyplot as plt
 import numpy as np
-from nltk.corpus import cmudict, stopwords
-from nltk.tokenize import word_tokenize, sent_tokenize
-
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 import nltk
 
-nltk.download('cmudict')
 nltk.download('stopwords')
 
 stop_words = set(stopwords.words('english'))
 
-
-def remove_special_chars(text):
-    return re.sub(r'[^a-zA-Z0-9\s]', '', text)
-
-def AverageSentenceLength(text) -> float:
-    # Tokenize sentences
-    sentences = sent_tokenize(text)
-    num_sentences = len(sentences)
-    
-    # Tokenize words
-    words = word_tokenize(text)
-    
-    # remove special characters from each word
-    cleaned_words = [remove_special_chars(word) for word in words if word.isalpha()]
-    num_words = len(cleaned_words)
-    
-    if num_sentences == 0 or num_words == 0:
-        return 0.0
-    
-    # Calculate ASL
-    return num_words / num_sentences
-
-def AverageSyllablesPerWord(text) -> float:
-    words = word_tokenize(text)
-    
-    # remove special characters from each word
-    cleaned_words = [remove_special_chars(word) for word in words if word.isalpha()]
-    
-    total_syllables = 0
-    total_words = len(cleaned_words)
-    
-    if total_words == 0:
-        return 0.0
-    
-    for word in cleaned_words:
-        total_syllables += textstat.syllable_count(word)
-    
-    return total_syllables / total_words
 
 # The FRE score is a number between 0 and 100, with higher scores indicating easier reading
 def FleschReadingEaseScore(text) -> float:
@@ -68,47 +25,24 @@ def FleschReadingEaseScore(text) -> float:
     # 30-49:  Difficult
     # 0-29:   Very confusing
     
-    ASL = AverageSentenceLength(text)
-    ASW = AverageSyllablesPerWord(text)
+    ASL = average_sentence_length(text)
+    ASW = average_syllables_per_word(text)
     
     return 206.835 - (1.015 * ASL) - (84.6 * ASW)
 
 # Indicates the education level required to understand the text.
 def FleschKincaidGradeLevel(text) -> float:
-    ASL = AverageSentenceLength(text)
-    ASW = AverageSyllablesPerWord(text)
+    ASL = average_sentence_length(text)
+    ASW = average_syllables_per_word(text)
     
     return 0.39 * ASL + 11.8 * ASW - 15.59
 
 def GunningFogIndex(text) -> float:
     # Gradelevel = 0.4(ASL + PHW)
     # PHW: The percentage of hard words
-    ASL = AverageSentenceLength(text)
-    PHW = PercentageHardWords(text)
+    ASL = average_sentence_length(text)
+    PHW = percentage_hard_words(text)
     return 0.4 * (ASL + PHW)
-
-def PercentageHardWords(text) -> float:
-    words = word_tokenize(text)
-    
-    # remove special characters from each word
-    cleaned_words = [remove_special_chars(word) for word in words if word.isalpha()]
-    
-    if len(cleaned_words) == 0:
-        return 0.0
-    
-    hard_word_count = 0
-    for word in cleaned_words:
-        if is_complex_word(word):
-            hard_word_count += 1
-    
-    return (hard_word_count / len(cleaned_words)) * 100
-
-def is_complex_word(word):
-    if re.match(r'(.*ing|.*ed|.*es|.*ly)$', word):
-        return False
-    
-    syllable_count = textstat.syllable_count(word)
-    return syllable_count >= 3
 
 def ShannonEntropy(text) -> float:
     from scipy.stats import entropy
@@ -158,24 +92,3 @@ def SimpsonsIndex(text) -> float:
     
 def DaleChallReadability(text) -> float:
     return textstat.dale_chall_readability_score(text)
-
-def RemoveHeaderText(text):
-    # The header of each book usually ends with '** anything **' or '*** anything ***' 
-    pattern = r'\*{2,}\s*.*?\s*\*{2,}'    
-    found = re.search(pattern, text)
-    
-    if found:
-        header_end_pos = found.end()
-        return text[header_end_pos:].strip()
-    return text
-
-def RemoveFooterText(text):
-    # The footer usually starts with '*** anything ***'
-    pattern = r'\*{3,}\s*.*?\s*\*{3,}'
-    
-    footer_matches = list(re.finditer(pattern, text))
-    if footer_matches:
-        last_match = footer_matches[-1]
-        footer_start_pos = last_match.start()
-        return text[:footer_start_pos].strip()
-    return text
--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-import os
+import os, json
 import google.generativeai as genai
 
 from FeatureMetrics import calculate_feature_metrics
-from FeatureExtraction import RemoveHeaderText, RemoveFooterText
+from helper import remove_header_text, remove_footer_text
 
 def main():
     current_directory = os.getcwd()
@@ -11,23 +11,29 @@ def main():
     parent_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
     print(f"Parent Directory: {parent_directory}\n")
     
-    # load writing sample
+    # Parse config file to get API key
+    with open('src/config.json', 'r') as file:
+        config = json.load(file)
+    
+    # Setup API
+    os.environ['GOOGLE_API_KEY'] = config.get('api_key')
+    genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+    model = genai.GenerativeModel("gemini-1.5-pro")
+    
+    # load writing sample 
     with open('datasets/ProjectGutenbergTop2000/books/3.txt', 'r', encoding='utf-8-sig') as file:
         text = file.read()
     
     # Remove header text
-    text = RemoveHeaderText(text)
+    text = remove_header_text(text)
     # Remove footer text
-    text = RemoveFooterText(text)
+    text = remove_footer_text(text)
     
+    # Extract feature metrics
     metrics = calculate_feature_metrics(text)
     print(metrics)
     
-    # Setup API
-    os.environ['GOOGLE_API_KEY'] = "API_KEY_HERE"
-    genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
-    model = genai.GenerativeModel("gemini-1.5-pro")
-    
+    # Prompt model to generate content with extracted metrics
     prompt = f"{metrics}\n Using these stylistic metrics write a paragraph about how you enjoy going on walks through the woods"
     response = model.generate_content(prompt)
     
